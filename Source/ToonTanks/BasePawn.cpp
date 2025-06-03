7bdaf95f0c8246ed968a9ff5d88195ee164a0719
Octavio Ortega <octaviortegan@hotmail.com>
2025-06-02 21:07:52 -0600

    Tanks Code Foundation

--- Source/ToonTanks/BasePawn.cpp
+++ Source/ToonTanks/BasePawn.cpp
@@ -6,8 +6,18 @@
 ABasePawn::ABasePawn()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
 	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
 	RootComponent = CapsuleComponent;
+
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh"));
+	BaseMesh->SetupAttachment(CapsuleComponent);
+
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret Mesh"));
+	TurretMesh->SetupAttachment(BaseMesh);
+
+	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Projectile Spawn"));
+	ProjectileSpawnPoint->SetupAttachment(TurretMesh);
 }
 
 void ABasePawn::BeginPlay()
